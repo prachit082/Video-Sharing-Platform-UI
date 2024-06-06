4585a72de30230a06b89d9d4d6ea1fcf809b7a52
Prachit Pandit <92202695+prachit082@users.noreply.github.com>
2024-06-06 22:21:50 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+/**
+ * Represents a collection of all video elements on the page.
+ * @type {NodeListOf<HTMLVideoElement>}
+ */
 const allVideos = document.querySelectorAll('video');
+/**
+ * Represents the sidebar element.
+ * @type {HTMLElement}
+ */
 const sidebar = document.querySelector('.left-section');
+/**
+ * Represents a collection of sidebar items.
+ * @type {NodeListOf<Element>}
+ */
 const sidebarItems = document.querySelectorAll('.sidebar .item');
+/**
+ * Represents a collection of category items.
+ * @type {NodeListOf<Element>}
+ */
 const catItems = document.querySelectorAll('.categories a');
 
 // Play/Pause video on mouse enter/leave
 allVideos.forEach(video => {
+    /**
+     * Play the video when the mouse enters.
+     * @event mouseover
+     */
     video.addEventListener('mouseover', () => {
         video.play();
     });
+    /**
+     * Pause the video when the mouse leaves.
+     * @event mouseleave
+     */
     video.addEventListener('mouseleave', () => {
         video.pause();
     });
@@ -15,6 +39,10 @@ allVideos.forEach(video => {
 
 // Handle sidebar item click
 sidebarItems.forEach(sideItem => {
+    /**
+     * Handle the click event on a sidebar item.
+     * @event click
+     */
     sideItem.addEventListener('click', () => {
         sidebarItems.forEach(item => {
             item.classList.remove('active');
@@ -25,6 +53,10 @@ sidebarItems.forEach(sideItem => {
 
 // Handle category item click
 catItems.forEach(catItem => {
+    /**
+     * Handle the click event on a category item.
+     * @event click
+     */
     catItem.addEventListener('click', () => {
         catItems.forEach(item => {
             item.classList.remove('active');
